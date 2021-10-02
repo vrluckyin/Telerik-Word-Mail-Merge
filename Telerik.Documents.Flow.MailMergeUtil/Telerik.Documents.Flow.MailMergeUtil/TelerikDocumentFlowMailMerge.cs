275d6b6f270d84cf4a7e848c30353e278d978dfb
Vipul Patel <vipul.patel@aisoftwarellc.com>
2021-10-02 14:31:31 +0530

    cleanup

--- Telerik.Documents.Flow.MailMergeUtil/Telerik.Documents.Flow.MailMergeUtil/TelerikDocumentFlowMailMerge.cs
+++ Telerik.Documents.Flow.MailMergeUtil/Telerik.Documents.Flow.MailMergeUtil/TelerikDocumentFlowMailMerge.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Telerik.Documents.Flow.MailMergeUtil.Model;
 using Telerik.Documents.Flow.MailMergeUtil.Tokenizer;
--- Telerik.Documents.Flow.MailMergeUtil/Telerik.Documents.Flow.MailMergeUtil/Tokenizer/MergeFieldTokenizerBase.cs
+++ Telerik.Documents.Flow.MailMergeUtil/Telerik.Documents.Flow.MailMergeUtil/Tokenizer/MergeFieldTokenizerBase.cs
@@ -1,19 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Dynamic;
-using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
-using Telerik.Documents.Common.Model;
 using Telerik.Documents.Flow.MailMergeUtil.Model;
 using Telerik.Documents.Flow.MailMergeUtil.TokenProcessors;
-using Telerik.Documents.Media;
-
-using Telerik.Documents.Primitives;
 using Telerik.Windows.Documents.Flow.Model;
-using Telerik.Windows.Documents.Flow.Model.Editing;
-using Telerik.Windows.Documents.Flow.Model.Shapes;
 
 namespace Telerik.Documents.Flow.MailMergeUtil.Tokenizer
 {
